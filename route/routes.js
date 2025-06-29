7b9598f12b82eb664b9166c4bd85303ee84cece4
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-29 17:42:50 +0600

    create app route and handler

---
+++ route/routes.js
@@ -0,0 +1,19 @@
+/**
+|--------------------------------------------------
+| Title: Routes for API Monitoring
+| Description: Application routes for handling API requests.
+| Author: Md. Suruj Miah
+| Email:surujmiah6896@gmail.com
+| License: MIT
+| Version: 1.0.0
+| Last Updated: 29/5/2025
+|--------------------------------------------------
+*/
+// Dependencies
+const homeHandler = '';
+
+const routes = {
+    home: homeHandler,
+};
+
+module.exports = routes;
