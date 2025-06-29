c7c5a67318dfaf25d7896e7f8445be49e608a18f
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-29 22:47:39 +0600

    user info get

--- handlers/routerHandlers/userHandler.js
+++ handlers/routerHandlers/userHandler.js
@@ -10,86 +10,136 @@
 |--------------------------------------------------
 */
 
-
 // module dependencies
-const handler = {}; 
-const data = require('../../lib/data');
-const { parseJsonToObject} = require('../../helpers/utilities');
-const { Hash} = require('../../helpers/utilities');
+const handler = {};
+const data = require("../../lib/data");
+const { parseJsonToObject } = require("../../helpers/utilities");
+const { Hash } = require("../../helpers/utilities");
 
 handler.userHandler = (requestProperties, callback) => {
-
-    const acceptedMethods = ['get', 'post', 'put', 'delete'];
-    if(acceptedMethods.indexOf(requestProperties.method) > -1) {
-        handler._users[requestProperties.method](requestProperties, callback);
-    } else {
-        callback(405, {
-            message: 'Method Not Allowed',
-            status: 'error',
-        });
-    }
+  const acceptedMethods = ["get", "post", "put", "delete"];
+  if (acceptedMethods.indexOf(requestProperties.method) > -1) {
+    handler._users[requestProperties.method](requestProperties, callback);
+  } else {
+    callback(405, {
+      message: "Method Not Allowed",
+      status: "error",
+    });
+  }
 };
- 
+
 // Container for all the users methods
-handler._users = {};    
+handler._users = {};
 
 // Users - post
 handler._users.post = (requestProperties, callback) => {
-    // Validate the inputs
-    const firstName = typeof requestProperties.body.firstName === 'string' && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false;
-    const lastName = typeof(requestProperties.body.lastName) === 'string' && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName.trim() : false;
-    const phone = typeof(requestProperties.body.phone) === 'string' && requestProperties.body.phone.trim().length === 11 ? requestProperties.body.phone.trim() : false;
-    const password = typeof(requestProperties.body.password) === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password.trim() : false;
-    const tosAgreement = typeof(requestProperties.body.tosAgreement) === 'boolean' && requestProperties.body.tosAgreement === true ? true : false;
-    
-    if(firstName && lastName && phone && password && tosAgreement) {
-        // Make sure that the user does not already exist
-        data.read('users', phone, (err, userData) => {
-            if(err) {
-                // Hash the password
-                const hashedPassword = Hash(password);
-                if(hashedPassword) {
-                    // Create the user object
-                    const userObject = {
-                        firstName,
-                        lastName,
-                        phone,
-                        password: hashedPassword,
-                        tosAgreement,
-                    };
-                    // Store the user
-                    data.create('users', phone, userObject, (err) => {
-                        if(!err) {
-                            callback(200, {
-                                message: 'User created successfully',
-                                status: 'success',
-                            });
-                        } else {
-                            callback(500, {
-                                message: 'Could not create the user',
-                                status: 'error',
-                            });
-                        }
-                    });
-                } else {
-                    callback(500, {
-                        message: 'Could not hash the user\'s password',
-                        status: 'error',
-                    });
-                }
+  // Validate the inputs
+  const firstName =
+    typeof requestProperties.body.firstName === "string" &&
+    requestProperties.body.firstName.trim().length > 0
+      ? requestProperties.body.firstName
+      : false;
+  const lastName =
+    typeof requestProperties.body.lastName === "string" &&
+    requestProperties.body.lastName.trim().length > 0
+      ? requestProperties.body.lastName.trim()
+      : false;
+  const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone.trim()
+      : false;
+  const password =
+    typeof requestProperties.body.password === "string" &&
+    requestProperties.body.password.trim().length > 0
+      ? requestProperties.body.password.trim()
+      : false;
+  const tosAgreement =
+    typeof requestProperties.body.tosAgreement === "boolean" &&
+    requestProperties.body.tosAgreement === true
+      ? true
+      : false;
+
+  if (firstName && lastName && phone && password && tosAgreement) {
+    // Make sure that the user does not already exist
+    data.read("users", phone, (err, userData) => {
+      if (err) {
+        // Hash the password
+        const hashedPassword = Hash(password);
+        if (hashedPassword) {
+          // Create the user object
+          const userObject = {
+            firstName,
+            lastName,
+            phone,
+            password: hashedPassword,
+            tosAgreement,
+          };
+          // Store the user
+          data.create("users", phone, userObject, (err) => {
+            if (!err) {
+              callback(200, {
+                message: "User created successfully",
+                status: "success",
+              });
             } else {
-                callback(400, {
-                    message: 'User already exists',
-                    status: 'error',
-                });
+              callback(500, {
+                message: "Could not create the user",
+                status: "error",
+              });
             }
-        });
-    } else {
+          });
+        } else {
+          callback(500, {
+            message: "Could not hash the user's password",
+            status: "error",
+          });
+        }
+      } else {
         callback(400, {
-            message: 'Invalid inputs',
-            status: 'error',
+          message: "User already exists",
+          status: "error",
+          user: userData,
         });
-    }
+      }
+    });
+  } else {
+    callback(400, {
+      message: "Invalid inputs",
+      status: "error",
+    });
+  }
+};
+
+// Users - get
+handler._users.get = (requestProperties, callback) => {
+  // Check the phone number
+  const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone.trim()
+      : false;
+  console.log("phone", phone);
+
+  if (phone) {
+    data.read("users", phone, (err, userData) => {
+      if (!err && userData) {
+        // Remove the password from the user object before returning it
+        // delete userData.password;
+        callback(200, userData);
+      } else {
+        callback(404, {
+          message: "User not found",
+          status: "error",
+        });
+      }
+    });
+  } else {
+    callback(400, {
+      message: "Invalid phone number",
+      status: "error",
+    });
+  }
 };
 
 // Export the handler
