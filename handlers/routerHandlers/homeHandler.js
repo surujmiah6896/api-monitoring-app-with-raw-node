d04939fc3a191d5f8d962a3862118f5d973f358c
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-29 17:45:47 +0600

    create home handler

---
+++ handlers/routerHandlers/homeHandler.js
@@ -0,0 +1,33 @@
+/**
+|--------------------------------------------------
+| Title: Home Handler
+| Description: Application routes for handling API requests.
+| Author: Md. Suruj Miah
+| Email:surujmiah6896@gmail.com
+| License: MIT
+| Version: 1.0.0
+| Last Updated: 29/5/2025
+|--------------------------------------------------
+*/
+
+// module dependencies
+const handler = {};
+
+handler.homeHandler = (requestProperties, callback) => {
+  const response = {
+    message: 'Welcome to the API Monitoring Service',
+    status: 'success',
+  };
+
+  // Check the request method
+  if (requestProperties.method === 'get') {
+    // Respond with a success message
+    callback(200, response);
+  } else {
+    // Respond with a method not allowed error
+    callback(405, {
+      message: 'Method Not Allowed',
+      status: 'error',
+    });
+  }
+}
