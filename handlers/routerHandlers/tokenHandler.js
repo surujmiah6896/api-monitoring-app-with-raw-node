37bcee5edb069b5270c556440aa9e315acfa67dd
surujmiah6896 <surujmiah6896@gmail.com>
2025-06-30 00:55:17 +0600

    create tokenHandle

--- handlers/routerHandlers/tokenHandler.js
+++ handlers/routerHandlers/tokenHandler.js
@@ -13,7 +13,16 @@
 // dependencies
 const data = require('../../lib/data');
 const { parseJsonToObject, Hash } = require('../../helpers/utilities');
-const { verifyToken } = require('../../helpers/utilities');
+const { createRandomString } = require('../../helpers/utilities');
 
 // Module dependencies
-const handle = {};
+const handler = {};
+
+handler.tokenHandler = (requestProperties, callback) => {
+  const acceptedMethods = ['post', 'get', 'put', 'delete'];
+  if (acceptedMethods.indexOf(requestProperties.method) > -1) {
+    handler._tokens[requestProperties.method](requestProperties, callback);
+  } else {
+    callback(405);
+  }
+}
